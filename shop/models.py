3c663df151f3cb0400fed08f39200ac9d12e8649
nivethadeva <nivetha2313@gmail.com>
2025-08-28 12:27:46 +0530

    prepare for clever

--- shop/models.py
+++ shop/models.py
@@ -10,7 +10,8 @@ class Product(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='products/')
-    description = models.TextField()
+    description = models.TextField(default='No description')
+
    
 
     def __str__(self):
