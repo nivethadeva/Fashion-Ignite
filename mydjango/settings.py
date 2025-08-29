ffd62be6b7040164ff702cef4d4156f4ddbd11d9
Nivetha D <nivetha2313@gmail.com>
2025-08-29 15:39:07 +0530

    Update settings.py

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'app-add66c9a-2357-42d0-be55-7f4ea91e0d65.cleverapps.io',
-    'nivetha.wuaze.com'
+    'app.nivetha.my-style.in'
 ]
 
 # ------------------------------
