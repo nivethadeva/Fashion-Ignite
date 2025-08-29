ea38b1cba9614cf2d4d8233ec83c7926ecdfa519
nivethadeva <nivetha2313@gmail.com>
2025-08-29 13:04:48 +0530

    Initial commit

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -14,10 +14,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security & Debug
 # ------------------------------
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-replace-me")
-DEBUG = False  # Always False in production
+DEBUG = True  # Always False in production
 
 # Allowed hosts: your Clever Cloud app URL
-ALLOWED_HOSTS = ['your-app-name.cleverapps.io']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'app-add66c9a-2357-42d0-be55-7f4ea91e0d65.cleverapps.io'
+]
 
 # ------------------------------
 # Installed apps
